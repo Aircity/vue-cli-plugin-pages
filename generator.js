7c0ab71150996d9404a266d3c8dea63e284b9a08
Aircity <aircity@drrr.us>
2018-09-15 13:10:26 +0800

    file-catalog

--- generator.js
+++ generator.js
@@ -1,7 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+
 module.exports = api => {
-  // Most likely you want to overwrite the whole config to ensure it's working
-  // without conflicts, e.g. for a project that used Jest without Babel.
-  // It should be rare for the user to have their own special babel config
-  // without using the Babel plugin already.
+
   api.render("./template");
+
+  const configPath =
+    process.env.VUE_CLI_SERVICE_CONFIG_PATH || api.resolve("vue.config.js");
+
+  if (!fs.existsSync(configPath)) {
+    try {
+      let template = fs.readFileSync(path.join(__dirname, "./lib/vue.config.js"));
+      fs.writeFile(configPath, template, function(err) {
+        if (err) {
+          return console.log(err);
+        }
+      });
+    } catch (e) {
+      throw e;
+    }
+  }
+
 };
