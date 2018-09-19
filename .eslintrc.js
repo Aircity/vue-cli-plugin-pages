fd3c4e417a2a2b691e5cc44a6245e99cd304c438
Aircity <aircity@drrr.us>
2018-09-19 21:19:40 +0800

    :bookmark:V0.72

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
     // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
     'standard',
     // https://github.com/vuejs/eslint-plugin-vue#priority-c-recommended-minimizing-arbitrary-choices-and-cognitive-overhead
-    'plugin:vue/essentials',
-  ]
+    'plugin:vue/recommended',
+  ],
+  rules: {
+    'vue/require-default-prop': 'off'
+  }
 };
