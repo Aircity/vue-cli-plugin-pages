32636184f73b97dbf9adfa2d57b73de3c614fecc
Aircity <aircity@drrr.us>
2018-09-19 20:29:31 +0800

    :art:Beautify

--- template/src/components/HelloWorld.vue
+++ template/src/components/HelloWorld.vue
@@ -2,25 +2,30 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      For guide and recipes on how to configure / customize this project,<br> check out the
+      <a href="https://cli.vuejs.org"
+         target="_blank"
+         rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Enter Appliction</h3>
+    <h3>Where You Are</h3>
     <ul>
-      <li><a href="preview.html" target="_blank" rel="noopener">Preview</a></li>
-      <li><a href="#" target="_blank" rel="noopener">Version</a></li>
+      <li>
+        <span>{{pathname}}</span>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  created() {
+    this.pathname = window.location.pathname;
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
