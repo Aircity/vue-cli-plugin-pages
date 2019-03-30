fb756eba47f2971ed3cbce8fb24672ff01a58cf6
Aircity <aircity@drrr.us>
2019-03-30 09:58:45 +0800

    doc:change prompts

--- prompts.js
+++ prompts.js
@@ -2,14 +2,15 @@ module.exports = [{
   name: 'generate',
   type: 'list',
   message: 'What do you want to generate?',
-  choices: [{
-      name: 'Initial framework',
-      value: 'init'
-    },
+  choices: [
     {
       name: 'Initial framework and generate examples',
       value: 'whole'
+    },
+    {
+      name: 'Initial framework only',
+      value: 'init'
     }
   ],
-  default: 'init',
+  default: 'init'
 }]
